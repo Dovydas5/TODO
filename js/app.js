ab283b65df56b469f8a6295afd4109788137026b
Dovydasp <59613922+Dovydasp@users.noreply.github.com>
2020-05-05 18:42:10 +0300

    gg

---
+++ js/app.js
@@ -0,0 +1,13 @@
+let todo_list = [
+        ['eiti', '2020-05-05', '2020-05-06', 'todo'],
+        ['begti', '2020-05-06', '2020-05-07', 'todo'],
+        ['rekti', '2020-05-07', '2020-05-08', 'todo']
+];
+const obj = {
+    description: 'eiti',
+    created_on: '2020-05-05',
+    deadline: '2020-05-06',
+    status: 'todo'
+}
+console.log(obj );
+console.log(' Obj description:', obj.description );
