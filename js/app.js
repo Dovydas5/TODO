2fc94e5000454929a208721d4c1924aaad9ad0aa
Dovydasp <59613922+Dovydasp@users.noreply.github.com>
2020-05-06 15:50:58 +0300

    test

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-let todo_list = [
-        ['eiti', '2020-05-05', '2020-05-06', 'todo'],
-        ['begti', '2020-05-06', '2020-05-07', 'todo'],
-        ['rekti', '2020-05-07', '2020-05-08', 'todo']
-];
-const obj = {
-    description: 'eiti',
-    created_on: '2020-05-05',
-    deadline: '2020-05-06',
-    status: 'todo'
-}
-console.log(obj );
-console.log(' Obj description:', obj.description );
+console.log(todo_list)
+
+ function renderList (list){
+    let HTML = '';
+    for( let i=0; i<list.length; i++ ){
+        const todoItem = list[i];
+        console.log(todoItem.description); 
+
+        }
+
+ }
+ renderList(todo_list);
---
+++ js/todos.js
@@ -0,0 +1,7 @@
+
+const todo_list = {
+description: 'eiti',
+created_on: '2020-05-05',
+deadline: '2020-05-06',
+status: 'todo'
+}
