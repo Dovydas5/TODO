4b70d6fd39a4fea3c6d049e80a286ff22f8e1b3e
Dovydasp <59613922+Dovydasp@users.noreply.github.com>
2020-05-11 15:23:56 +0300

    add and clear

--- js/app.js
+++ js/app.js
@@ -21,15 +21,64 @@ function renderList(list) {
 
 renderList(todo_list);
 
-const removeActions = document.querySelectorAll('.remove')
+const removeActions = document.querySelectorAll(".item .actions.remove");
 
+for (let i = 0; i < removeActions.length; i++) {
+  const removeElement = removeActions[i];
 
-for ( let i=0; i<removeActions.length; i++){
-  const removeElement = removeActions[i]
-
-  removeElement.addEventListener('click', removeTodo())
+  removeElement.addEventListener("click", removeTodo);
 }
 
-function removeTodo(){
-  console.log('ggw')
+function removeTodo(event) {
+  const parentItem = event.target.closest(".item");
+  parentItem.remove();
+}
+
+// const removeAll = document.querySelector('.global > action.remove');
+
+// removeAll.addEventListener('click', deleteAll);
+
+// function deleteAll (event) {
+//   const parent = event.target.closes('.container')
+//   const AllTodo = parent.querySelectorAll('item')
+//   for ( let i=0; i<AllTodo.length; i++){
+//     AllTodo[i].remove();
+//   }
+// }
+
+const form = document.querySelector("form");
+const taskTextarea = form.querySelector("textarea");
+const deadline = form.querySelector("input");
+const formActions = form.querySelector("actions");
+const formAdd = document.querySelector(".btn.add");
+const formClear = document.querySelector(".btn.clear");
+
+formClear.addEventListener("click", clearForm);
+function clearForm() {
+  taskTextarea.value = "";
+  deadline.value = "";
+}
+
+formAdd.addEventListener("click", addNewTodoItem);
+function addNewTodoItem() {
+  let newTodo = {
+    description: taskTextarea.value.trim(),
+    created_on: "2020-05-05",
+    deadline: deadline.value.trim(),
+    status: "todo",
+  };
+
+  if (newTodo.description.length === 0 ) {
+    return console.log("tuscia");
+  }
+  if (newTodo.deadline.length > 0 &&  new Date(newTodo.deadline).toString() === 'Invalid Date' ){
+    return console.log("bloga reiskme");
+
+
+  }
+  
+
+
+  console.log(newTodo);
+  return;
 }
