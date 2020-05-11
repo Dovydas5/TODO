3462229589b5b748a4867b635e754e8fadd081ac
Dovydasp <59613922+Dovydasp@users.noreply.github.com>
2020-05-11 13:15:21 +0300

    update

--- js/app.js
+++ js/app.js
@@ -11,8 +11,25 @@ function renderList(list) {
     <div class="status ${todoItem.status}"></div>
     <p class="description">${todoItem.description}</p>
     <div class="deadline">${todoItem.deadline}</div>
+    <div class="actions">
+        <div class="remove actions">Remove</div>
+</div>
     </div>`;
   }
   return (listTodo.innerHTML += HTML);
 }
+
 renderList(todo_list);
+
+const removeActions = document.querySelectorAll('.remove')
+
+
+for ( let i=0; i<removeActions.length; i++){
+  const removeElement = removeActions[i]
+
+  removeElement.addEventListener('click', removeTodo())
+}
+
+function removeTodo(){
+  console.log('ggw')
+}
